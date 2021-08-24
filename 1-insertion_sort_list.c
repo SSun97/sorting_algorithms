64e565f6ddf086b08713849926846c355cd95597
ssun97 <sunmingyu.97@gmail.com>
2021-08-24 23:41:26 +0000

    task 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort the double linked list
+ * @list: linked list
+ * 
+ * Return: no return
+ */
+
+
+void insertion_sort_list(listint_t **list) {
+
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * swap - swap two integers
+ * @xp: first integer
+ * @yp: second integer
+ * Return: no return
+ */
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ * selection_sort - sort the array
+ * @array: provide array
+ * @size: the length of the array
+ * Return: no return
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+		swap(&array[min_idx], &array[i]);
+		print_array(array, size);
+	}
+}
