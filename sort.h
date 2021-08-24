d55c533bcc144f2def947975bcdd6f3c561fa276
ssun97 <sunmingyu.97@gmail.com>
2021-08-24 17:40:47 +0000

    betty check

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -24,3 +24,4 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 
 #endif
+
