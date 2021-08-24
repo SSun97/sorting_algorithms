a58fe70a4ec7314d49da5235dfa202449877f028
ssun97 <sunmingyu.97@gmail.com>
2021-08-24 17:38:43 +0000

    betty check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,30 @@
 #include "sort.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * bubble_sort - sort the array
+ * @array: array
+ * @size: size of arraw
+ * Return: no return
  */
 
 void bubble_sort(int *array, size_t size)
 {
-  size_t i = 0;
-  size_t j = 0;
+	size_t i = 0;
+	size_t j = 0;
 
-  for(i = 0; i < size; i++)
-  {
-    for(j = 0; j < size - 1- i; j++)
-    {
-      if(array[j] > array[ j+ 1])
-      {
-        int temp = 0;
-        
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-        print_array(array, size);
-      }
-    }
-  }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = 0;
+
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
