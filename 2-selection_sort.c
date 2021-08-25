5fb7a4bec752e3000056886ca3e31abbe782438a
ssun97 <sunmingyu.97@gmail.com>
2021-08-25 02:53:47 +0000

    only print when change the index

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -28,11 +28,14 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
+
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_idx])
 				min_idx = j;
 
 		swap(&array[min_idx], &array[i]);
-		print_array(array, size);
+
+		if (min_idx != i)
+			print_array(array, size);
 	}
 }
